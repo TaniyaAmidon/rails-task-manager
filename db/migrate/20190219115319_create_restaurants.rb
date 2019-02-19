7f1a8d975710f7aab091064260a0d8495618cf5c
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-02-19 11:58:23 +0000

    restaurant model created

---
+++ db/migrate/20190219115319_create_restaurants.rb
@@ -0,0 +1,12 @@
+class CreateRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restaurants do |t|
+      t.string :title
+      t.text :details
+      t.boolean :completed
+      t.false :default
+
+      t.timestamps
+    end
+  end
+end
